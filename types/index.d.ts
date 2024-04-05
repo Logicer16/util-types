a8d5817272ef826702f351a154a169efeece7e1f
Logicer <logicer@logicer.cc>
2024-04-05 21:16:35 +1100

    fix formatting

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,13 +4,13 @@
 /* eslint-disable import/export */
 
 /**
- * WARNING! Contains internal types used to produces the types exported by this package. These types are unsupported and may change at any time. USE AT YOUR OWN RISK
+ * WARNING! Contains internal types used to produces the types exported by this package. These types are unsupported and may change at any time. USE AT YOUR OWN RISK.
  */
 export namespace Unsafe {
   export type {Unsafe as UnionMax} from "./UnionMax.d.ts";
   export type {Unsafe as IntRange} from "./IntRange.d.ts";
   /**
-   * From [`@logicer/ts-arithmetic`](https://www.npmjs.com/package/@logicer/ts-arithmetic)
+   * From [`@logicer/ts-arithmetic`](https://www.npmjs.com/package/@logicer/ts-arithmetic).
    */
   export type {Unsafe as Arithmetic} from "@logicer/ts-arithmetic";
 }
